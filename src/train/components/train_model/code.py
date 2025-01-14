f2640cba17cf368af584fb36b28da1867097210e
diogoazevedo15 <68329635+diogoazevedo15@users.noreply.github.com>
2025-01-14 15:57:30 +0000

    Update code.py

--- src/train/components/train_model/code.py
+++ src/train/components/train_model/code.py
@@ -2,7 +2,7 @@ def main():
     from shared.utils import utils_fun
     print("Hello from train-model")
     utils_fun
-    # Mockup changes 10
+    # Mockup changes 11
 
 if __name__ == "__main__":
     main()
